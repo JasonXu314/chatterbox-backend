7fb838ae7169af162bde90eb6a46e7af60f112dd
Jason Xu <Xinwen31415@gmail.com>
2023-02-14 09:50:59 -0600

    changed default port

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 	app.useWebSocketAdapter(new WsAdapter(app));
 	app.enableCors({ origin: true });
 
-	await app.listen(process.env.PORT || 5000);
+	await app.listen(process.env.PORT || 8888);
 }
 
 bootstrap();
